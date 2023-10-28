72d2b0bde5873f183f1d7ce653e315467046bd2f
Chen <chenmu10@gmail.com>
2023-10-28 22:16:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Testing AKS+ACR+GitHub Actions
+Sample repo for testing things
---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Having fun with GitHub Actions</title>
+ </head>
+ <body>
+<body bgcolor="#f1f1f1" text="Blue">
+<img src="https://shaystorageghactions.blob.core.windows.net/public/0919.sdt-github.gif" alt="Docker">
+<h1>Hi TLV !If you're seeing this, your website works perfectly!!! :-)</h1>
+<?php
+?> 
+ </body>
+</html>
